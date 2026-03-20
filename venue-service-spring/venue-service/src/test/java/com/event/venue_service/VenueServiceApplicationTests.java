6d7654dcd0dad7dfc337def87f6a06cb23b32dd6
SatyabrataPanda42 <satyabratapandaaims@gmail.com>
2026-03-20 21:49:04 +0530

    trying testing

--- venue-service-spring/venue-service/src/test/java/com/event/venue_service/VenueServiceApplicationTests.java
+++ venue-service-spring/venue-service/src/test/java/com/event/venue_service/VenueServiceApplicationTests.java
@@ -1,13 +1,50 @@
 package com.event.venue_service;
 
+import com.event.venue_service.controller.VenueController;
+import com.event.venue_service.model.Venue;
+import com.event.venue_service.service.VenueService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class VenueServiceApplicationTests {
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(VenueController.class)
+class VenueControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private VenueService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(username = "vendor1", authorities = {"vendor"})
+    void createVenue_success() throws Exception {
+
+        Venue venue = new Venue();
+        venue.setName("Test Venue");
+
+        when(service.createVenue(any())).thenReturn(venue);
+
+        mockMvc.perform(post("/venues")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(venue)))
+                .andExpect(status().isOk());
+
+        verify(service).createVenue(any());
+    }
 }
